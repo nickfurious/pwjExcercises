49472f6d1ee11adfdc2219e740fd162496e619a2
Nick Hale <datfuriouskid@live.com>
2020-09-29 17:24:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+// function addsomenumbers(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(addsomenumbers(2, 5));
+// console.log(addsomenumbers(20, 50));
+
+// function minconverttosec(minutes){
+//     return minutes * 60;
+// }
+
+// console.log(minconverttosec(1));
+// console.log(minconverttosec(4));
+
+// function myAgegInSeconds(age){
+//     return age * 12 * 30 * 24 * 60 * 60;
+// }
+
+// // console.log(myAgegInSeconds(39));
+
+// function getFirstItem(items) {
+//     return items[0]
+// }
+//     let items = [
+//         'banana', 'ninja', 
+//         'rza', 'gza'
+//     ]
+
+// // console.log(getFirstItem(items));
+
+
+
+// function badOrGoodMovie(movie_rating) {
+//     //less than or equal to 7?
+//     //bad movies 
+//     //good movies
+//     //put them in two differnt buckets
+//     let movie_result;
+
+//     if (movie_rating >= 7) {
+//         movie_result = 'Good movie'
+//     } else {
+//         movie_result = 'Bad movie'
+//         }
+
+//         return movie_result;
+//     }
+
+//     // console.log(badOrGoodMovie(6));
+//     // console.log(badOrGoodMovie(8));
+//     // console.log(badOrGoodMovie(9.2));
+//     // console.log(badOrGoodMovie(2));
+
+//     function isEmpty(some_string) {
+//         if (some_string == "") {
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+
+//     // console.log(isEmpty(""));
+//     // console.log(isEmpty("Enter the Dragon"));
+
+//     function findMin(numbers) {
+//         let minimum = numbers[0];
+//         for (let i = 0; i < numbers.length; i++) {
+//             if (numbers[i] < minimum) {
+//                 minimum = numbers[i]
+//             }
+//         }
+
+//         return minimum
+//     }
+
+//     // console.log(findMin([5, 2, 8, 9, 3, 7]));
+
+//     function findMax(numbers) {
+//         let maximum = numbers[0];
+//         for (let i = 0; i < numbers.length; i++) {
+//             if (numbers[i] > maximum) {
+//                 maximum = numbers[i]
+//             }
+//         }
+
+//         return maximum
+//     }
+
+//     console.log(findMax([5, 2, 8, 9, 3, 7, 13]));
+
+function sortBestRatingsFirst(numbers) {
+
+}
